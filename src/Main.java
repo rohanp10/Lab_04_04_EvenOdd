6e25396c93d61ada456d23ec9d00301f513c796d
rohanparikh <parikhrs@mail.uc.edu>
2023-06-12 21:31:16 -0400

    Basic Working Code

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        
+
+        int numToExamine = 4;
+
+        if (numToExamine % 2 == 0)
+        {
+            System.out.println("The number " + numToExamine + " is even");
+        }
+        else
+        {
+            System.out.println("The number " + numToExamine + " is odd");
+        }
+
     }
 }
